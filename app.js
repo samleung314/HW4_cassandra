6a0e0e4c0fd398837bb78cd6222c2afdd911b60f
samleung314 <samleung314@gmail.com>
2018-03-18 02:36:14 -0400

    /deposit query

--- app.js
+++ app.js
@@ -9,22 +9,26 @@ var async = require('async');
 //Connect to the cluster
 var client = new cassandra.Client({ contactPoints: ['127.0.0.1'], keyspace: 'hw4' });
 
+// multer
+var multer  = require('multer');
+var multipart = multer();
+
 //create express app
 var app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/deposit', function (req, res) {
-  console.log(req.stack[0]);
-  // // Use query markers (?) and parameters
-  // const query = 'INSERT INTO imgs (filename, contents) VALUES (?,?)';
-  // const params = [req.body.filename, req.body.contents];
-  // // Set the prepare flag in the query options
-  // console.log('EXECUTE');
-  // client.execute(query, params, { prepare: true })
-  //   .then(result => console.log('Row updated on the cluster'));
-  //   res.sendStatus(200);
+app.post('/deposit', upload.single('contents'), function (req, res) {
+  console.log(req);
+  // Use query markers (?) and parameters
+  const query = 'INSERT INTO imgs (filename, contents) VALUES (?,?)';
+  const params = [req.body.filename, req.file];
+  // Set the prepare flag in the query options
+  console.log('EXECUTE');
+  client.execute(query, params, { prepare: true })
+    .then(result => console.log('Row updated on the cluster'));
+    res.sendStatus(200);
 })
 
 app.get('/retrieve', function (req, res) {
