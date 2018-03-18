444128864eb8dc2d4e1aad458ba1a4b88e08f05a
samleung314 <samleung314@gmail.com>
2018-03-18 02:06:51 -0400

    /deposit query

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var cassandra = require('cassandra-driver');
 var async = require('async');
 
 //Connect to the cluster
-var client = new cassandra.Client({ contactPoints: ['127.0.0.1'], keyspace: 'imgs' });
+var client = new cassandra.Client({ contactPoints: ['127.0.0.1'], keyspace: 'hw4' });
 
 //create express app
 var app = express();
@@ -22,7 +22,7 @@ app.post('/deposit', function (req, res) {
   // Set the prepare flag in the query options
   client.execute(query, params, { prepare: true })
     .then(result => console.log('Row updated on the cluster'));
-
+    
 })
 
 app.get('/retrieve', function (req, res) {
