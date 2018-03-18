3c73101f50e50a1ee0a1caba2287f397a6c3412f
samleung314 <samleung314@gmail.com>
2018-03-18 03:50:20 -0400

    /deposit query

--- app.js
+++ app.js
@@ -20,23 +20,28 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-var last;
-
 app.post('/deposit', multipart.single('contents'), function (req, res) {
   // Use query markers (?) and parameters
   const query = 'INSERT INTO imgs (filename, contents) VALUES (?,?)';
   var fileBin = fs.readFileSync(req.file.path);
   const params = [req.body.filename, fileBin];
   // Set the prepare flag in the query options
-  last = req.file.path.toString();
-  console.log(last);
+
   client.execute(query, params, { prepare: true })
     .then(result => console.log('Uploaded ' + params[0]));
   res.sendStatus(200);
 })
 
 app.get('/retrieve', multipart.single('contents'), function (req, res) {
-  console.log(req.query.filename);
+  console.log("File:" + req.query.filename);
+
+  // Use query markers (?) and parameters
+  const query = 'SELECT contents FROM imgs WHERE filename=?';
+  const params = [req.query.filename];
+  // Set the prepare flag in the query options
+  client.execute(query, params, { prepare: true })
+    .then(result => console.log('Selected ' + params[0]));
+  
   res.writeHead(200, {
     'Content-Type': 'image/jpeg',
   });
