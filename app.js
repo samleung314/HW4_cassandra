1a802cef4141fbe674ad71c9b24983e4172fa144
samleung314 <samleung314@gmail.com>
2018-03-18 03:20:43 -0400

    /deposit query

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.post('/deposit', multipart.single('contents'), function (req, res) {
   var fileBin = fs.readFileSync(req.file.path);
   const params = [req.body.filename, fileBin];
   // Set the prepare flag in the query options
-  last = Object.assign({}, fileBin);
+  last = Object.assign({}, params[1]);
   client.execute(query, params, { prepare: true })
     .then(result => console.log('Uploaded ' + params[0]));
   res.sendStatus(200);
@@ -38,7 +38,9 @@ app.get('/retrieve', function (req, res) {
   res.writeHead(200, {
     'Content-Type': 'image/jpeg',
   });
-  res.sendfile(last);
+  var readStream = fs.createReadStream(last);
+    // We replaced all the event handlers with a simple call to readStream.pipe()
+    readStream.pipe(res);
 })
 
 module.exports = app;
