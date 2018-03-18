026fdb8367ba9669cea8e2105d7cbea763ccc34b
samleung314 <samleung314@gmail.com>
2018-03-18 03:11:48 -0400

    /deposit query

--- app.js
+++ app.js
@@ -20,14 +20,17 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+var last;
+
 app.post('/deposit', multipart.single('contents'), function (req, res) {
   // Use query markers (?) and parameters
   const query = 'INSERT INTO imgs (filename, contents) VALUES (?,?)';
   var fileBin = fs.readFileSync(req.file.path);
   const params = [req.body.filename, fileBin];
   // Set the prepare flag in the query options
+  last = Object.assign({}, fileBin);
   client.execute(query, params, { prepare: true })
-    .then(result => console.log('Row updated on the cluster'));
+    .then(result => console.log('Uploaded ' + params[0]));
   res.sendStatus(200);
 })
 
@@ -35,7 +38,7 @@ app.get('/retrieve', function (req, res) {
   res.writeHead(200, {
     'Content-Type': 'image/jpeg',
   });
-  res.end();
+  res.end(last, 'binary');
 })
 
 module.exports = app;
