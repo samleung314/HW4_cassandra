4c2e1c8b381794459f2b4011be778e65cff65de0
samleung314 <samleung314@gmail.com>
2018-03-18 01:44:03 -0400

    debug request body

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/deposit', function (req, res) {
-  console.log("FILE: " + req.body);
+  console.log("FILE: " + req.body.toString());
   res.sendStatus(200);
 })
 
