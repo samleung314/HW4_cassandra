c0d1eebcddca4c9072ac32cad848ff832d84789c
samleung314 <samleung314@gmail.com>
2018-03-18 01:40:30 -0400

    send status 200 to server

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/deposit', function (req, res) {
-  console.log("FILE: " + req.body.filename.toString());
+  console.log("FILE: " + req.body.filename);
   res.sendStatus(200);
 })
 
